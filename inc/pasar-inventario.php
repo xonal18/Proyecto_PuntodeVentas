a9d46621baaf262497e761e22b7d579d84d47d6a
xonal18 <152057390+xonal18@users.noreply.github.com>
2023-11-27 18:16:19 -0600

    Add files via upload

---
+++ inc/pasar-inventario.php
@@ -0,0 +1,7 @@
+<?php
+
+$foo = dirname(__DIR__) . "/modulos/inventario.csv";
+echo $foo;
+$bar = file($foo);
+echo "<pre>";
+var_dump($bar);
---
+++ js/productos-en-stock.js
@@ -0,0 +1,90 @@
+/**
+ * Created by parzibyte on 01/08/2016.
+ */
+$(principal);
+function principal() {
+    consulta_todo();
+    escuchar_elementos();
+}
+function consulta_todo() {
+    consulta_familias(function () {
+        consultar_todos_los_productos_en_stock($("#familia").val());
+    });
+}
+function consultar_todos_los_productos_en_stock(familia) {
+    $.post("./modulos/inventario/consultar_todos_los_productos_en_stock.php",
+        {
+            familia: familia
+        },
+        function (respuesta) {
+            dibujar_tabla(respuesta.decodificar());
+        });
+}
+function consulta_familias(callback) {
+    $.get("./modulos/familias/consultar_familias_en_productos.php", function (familias) {
+        var f = JSON.parse(familias);
+        llenar_select(f);
+        callback();
+    });
+}
+function escuchar_elementos() {
+    $("#familia").change(function () {
+        consultar_todos_los_productos_en_stock($(this).val());
+    });
+
+    $("#generar_reporte_excel").click(async function () {
+        const url = `./modulos/reportes/stock.php`;
+        const payload = {
+            familia: $("#familia").val(),
+        };
+        const respuesta = await fetch(url, {
+            method: "POST",
+            credentials: 'include',
+            body: JSON.stringify(payload),
+        });
+        const contenidoDelArchivo = await respuesta.blob();
+        const a = document.createElement("a");
+        const objectUrl = URL.createObjectURL(contenidoDelArchivo);
+        a.href = objectUrl;
+        a.download = "Reporte stock.xlsx";
+        a.click();
+        URL.revokeObjectURL(url);
+    });
+}
+function llenar_select(familias) {
+    var $contenedorFamilia = $("#familia");
+    $contenedorFamilia
+        .empty()
+        .append(
+            $("<option>", {
+                val: "*",
+                text: "--Todas--"
+            })
+        );
+    familias.forEach(function (familia) {
+        $contenedorFamilia.append(
+            $("<option>", {
+                val: familia.familia,
+                text: familia.familia
+            })
+        );
+    });
+}
+function dibujar_tabla(datos) {
+    var longitud = datos.length,
+        $cuerpoTabla = $("#cuerpo_tabla");
+    $cuerpoTabla.empty();
+    for (var x = 0; x < longitud; x++) {
+        $cuerpoTabla.append(
+            $("<tr>")
+                .append(
+                    $("<td>").html(datos[x].codigo),
+                    $("<td>").html(datos[x].nombre),
+                    $("<td>").html(datos[x].existencia),
+                    $("<td>").html(datos[x].stock),
+                    $("<td>").html(datos[x].familia)
+                )
+        );
+    }
+
+}
